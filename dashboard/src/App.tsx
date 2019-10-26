84fd957db4ee26a257a0b35ca26ea0abfe462102
Yarin <yarinvak@gmail.com>
2019-10-26 14:19:09 +0300

    updated

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import DashBoard from 'graphql-vision-dashboard';
+import DashBoard from 'graphql-vision-react-dashboard';
+import {Helmet} from "react-helmet";
 
 const App: React.FC = () => {
 
     return (
         <div>
-            <DashBoard endpoint="http://localhost:4000/graphql" serviceName="Books Repository"/>
+            <Helmet>
+                <title>GraphQL Vision</title>
+            </Helmet>
+            <DashBoard endpoint="http://localhost:4000/graphql" serviceName="GraphQL Vision"/>
         </div>
     );
 };

569609d82232cbd6ed34658b9e2ab171a8af004f
Arik <furmanmail@gmail.com>
2020-01-12 17:18:11 +0200

    add senderId

--- packages/graphql-vision-plugin/src/graphql-vision-plugin.ts
+++ packages/graphql-vision-plugin/src/graphql-vision-plugin.ts
@@ -1,20 +1,25 @@
 import {request} from 'graphql-request';
 
-const tracingRequest = `mutation($tracing: TracerInput) {
-  addTracing(tracing: $tracing)
+const tracingRequest = `mutation($tracing: TracerInput!, $senderId: String) {
+  addTracing(tracing: $tracing, senderId: $senderId)
 }`;
 
 export default class GraphQLVisionPlugin {
     static endpoint: string;
+    static senderId: string;
 
-    constructor(endpointUrl: string) {
+    constructor(endpointUrl: string, senderId: string) {
         GraphQLVisionPlugin.endpoint = endpointUrl;
+        GraphQLVisionPlugin.senderId = senderId;
     }
 
     requestDidStart({}) {
         return {
             willSendResponse({response}: { response: any }) {
-                request(GraphQLVisionPlugin.endpoint, tracingRequest, {tracing: response.extensions.tracing}).then(() => {
+                request(GraphQLVisionPlugin.endpoint, tracingRequest, {
+                    tracing: response.extensions.tracing,
+                    senderId: GraphQLVisionPlugin.senderId
+                }).then(() => {
                 }).catch(err => console.log(err));
             }
         }

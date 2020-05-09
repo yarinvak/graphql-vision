2c9d19abedad2774488d91dcbd316bd14858fb10
Yarin <yarinvak@gmail.com>
2020-05-09 13:41:30 +0300

    fix: make duration a bigint

--- src/db/entities/tracing-result.ts
+++ src/db/entities/tracing-result.ts
@@ -17,7 +17,7 @@ export class TracingResult {
     @Column("timestamp")
     endTime: string;
 
-    @Column("int")
+    @Column("bigint")
     duration: number;
 
     @Column({type: "simple-json", nullable: true})

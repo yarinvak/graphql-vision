ef36f0d0e4212e23ebdc086479d0bc46d78b7ae3
Yarin <yarinvak@gmail.com>
2020-05-09 14:59:01 +0300

    fix: error messages

--- packages/graphql-vision-plugin/src/keep-alive-plugin.ts
+++ packages/graphql-vision-plugin/src/keep-alive-plugin.ts
@@ -1,14 +1,18 @@
 import {request} from 'graphql-request';
 import GraphQLVisionPlugin from "./graphql-vision-plugin";
+
 const keepAliveRequest = `mutation($senderId: String!) {
   keepAlive(senderId: $senderId)
 }`;
 
 export const keepAliveInterval = (intervalInMillis: number) => {
-    setInterval(() => {
-        request(GraphQLVisionPlugin.endpoint, keepAliveRequest, {
-            senderId: GraphQLVisionPlugin.senderId
-        }).then(() => {
-        }).catch();
-    }, intervalInMillis?? 5000)
+    setInterval(async () => {
+        try {
+            await request(GraphQLVisionPlugin.endpoint, keepAliveRequest, {
+                senderId: GraphQLVisionPlugin.senderId
+            });
+        } catch (e) {
+            throw new Error("Keep alive interval failed sending a keep-alive to the GraphQL-Vision Server. Is your vision server active?");
+        }
+    }, intervalInMillis ?? 5000)
 };
